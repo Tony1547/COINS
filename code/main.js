570c99b4648b719c596a6352edab1d08001d9319
Tony1547 <155920933+Tony1547@users.noreply.github.com>
2025-10-05 08:22:59 -0500

    bean

--- code/main.js
+++ code/main.js
@@ -1,30 +1,15 @@
 import kaplay from "kaplay"; 
 
-kaplay();
-loadBean();
-setGravity(1600);
+const k = kaplay({
+    width: 1280,
+    height: 720,
 
-const player = add([
-
-    sprite("bean"),
-    pos(center()),
-    area(),
-    body(),
-]);
-
-add([
-
-    rect(width(), 48),
-    outline(4),
-    area(),
-    pos(0, height() -48),
-    body({ isStatic: true})
-]);
-
-onKeyPress("space", () => {
-
-    if (player.isGrounded()) {
-        player.jump();
-    }
 });
+
+k.loadBean();
+
+const player = k.add([k.sprite("bean")]);
+
+
+
 
