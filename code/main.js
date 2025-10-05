819c933bc886ff7ad38b79b4f1b49d2c26f2b215
Tony1547 <155920933+Tony1547@users.noreply.github.com>
2025-10-05 08:46:07 -0500

    Update main.js

--- code/main.js
+++ code/main.js
@@ -7,9 +7,30 @@ const k = kaplay({
 });
 
 k.loadBean();
+k.setGravity(1000);
 
-const player = k.add([k.sprite("bean")]);
+//Button Press = jump
+player.onKeyPress("space", () => {
+    if (player.isGrounded) {player.jump();
+    }
+});
 
+//Player Character Made
+const player = k.add([
+    k.sprite("bean"),
+    k.pos(k.center()),
+    k.area(),
+    k.body(),
+]);
 
+//Box
+k.add([
+    k.rect(k.width(), 300), 
+    k.pos(0, 500), 
+    k.area(),
+    k.outline(3), 
+    k.body({isStatic: true})
+]);
 
+k.loop(1, )
 
