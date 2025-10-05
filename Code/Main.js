c848048cefff37fd4c838f71ba22cf3856737f9d
Tony1547 <155920933+Tony1547@users.noreply.github.com>
2025-10-05 05:38:18 -0500

    Update Main.js

--- Code/Main.js
+++ Code/Main.js
@@ -1,2 +1,11 @@
-let myVariable = 4;
-console.log(myVariable);
+const age = 25
+const height = 183
+
+
+if (age > 18) {
+    console.log("aduly");
+} else if (age == 18) {
+    console.log("18")
+} else {
+    console.log("minor");
+}
